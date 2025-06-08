f6b80e5ac7da88dbf83411ca7e9a50969ba47437
J-Uptegraph <johnnyup2@gmail.com>
2025-06-08 19:19:07 -0400

    First attempt at color sorting using OpenCV

--- cori_ws/src/cori_cv/launch/laundry_color_detector.launch.py
+++ cori_ws/src/cori_cv/launch/laundry_color_detector.launch.py
@@ -1,5 +1,10 @@
+import os
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+
+share_dir = get_package_share_directory('cori_cv')
+config_path = os.path.join(share_dir, 'color_piles.yaml')
 
 def generate_launch_description():
     return LaunchDescription([
@@ -7,6 +12,7 @@ def generate_launch_description():
             package='cori_cv',
             executable='laundry_color_detector',
             name='laundry_color_detector',
-            output='screen'
+            output='screen',
+            parameters=[{'config_path': config_path}]
         )
     ])
