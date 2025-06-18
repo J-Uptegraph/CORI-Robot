b89ca788c7a87cc72ce8c7805b00101e0a70707e
J-Uptegraph <johnnyup2@gmail.com>
2025-06-17 20:16:33 -0400

    Attempt 2 at CORI Launch!

--- cori_ws/build/cori_cv/share/cori_cv/hook/pythonpath_develop.ps1
+++
@@ -1,3 +0,0 @@
-# generated from colcon_powershell/shell/template/hook_prepend_value.ps1.em
-
-colcon_prepend_unique_value PYTHONPATH "$env:COLCON_CURRENT_PREFIX\/home/juptegraph/Workspaces/Robotics/Projects/CORI/cori_ws/build/cori_cv"
--- cori_ws/install/cori_cv/lib/python3.10/site-packages/cori_cv/__init__.py
+++
@@ -1,5 +0,0 @@
-# cori_cv package initializer
-# This makes cori_cv a valid Python package for ROS 2 and colcon
-
-# Optional: You can import key symbols here if desired, like:
-# from .nodes import laundry_color_detection
